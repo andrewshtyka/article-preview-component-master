4234e6758ac269db59364bee2d852d15e12e1246
Andrew Shtyka <andrii.shtyka@gmail.com>
2025-08-22 12:20:21 +0300

    finalize tooltip logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
 const buttonShare = document.getElementById("share-button");
 const tooltip = document.getElementById("tooltip");
 
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+//
+// initial tooltip settings
 const settings = {
   placement: "top",
   modifiers: [
@@ -13,9 +18,69 @@ const settings = {
   ],
 };
 
-//////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
 //
-// show Popper tooltip when width >=48rem
+// show tooltip logic
+function showTooltip() {
+  tooltip.setAttribute("data-show", "");
+  buttonShare.classList.add("is-active");
+
+  const markup = `
+<p class="c-font_preset-4">Share</p>
+<div class="o-flex_row u-flex_gap:1rem">
+  <button class="c-button_small" id="fb-button" type="button">
+    <svg class="c-icon_button:tooltip" height="20" width="20">
+      <use href="./images/icons.svg#icon-facebook" />
+    </svg>
+  </button>
+  <button class="c-button_small" id="tw-button" type="button">
+    <svg class="c-icon_button:tooltip" height="20" width="20">
+      <use href="./images/icons.svg#icon-twitter" />
+    </svg>
+  </button>
+  <button class="c-button_small" id="pi-button" type="button">
+    <svg class="c-icon_button:tooltip" height="20" width="20">
+      <use href="./images/icons.svg#icon-pinterest" />
+    </svg>
+  </button>
+</div>
+<div id="arrow" data-popper-arrow></div>
+`;
+
+  tooltip.insertAdjacentHTML("beforeend", markup);
+
+  // important part! points arrow to button, which called the tooltip
+  const arrowElement = tooltip.querySelector("[data-popper-arrow]");
+  popperInstance.setOptions({
+    placement: "top",
+    modifiers: [
+      { name: "offset", options: { offset: [0, 32] } },
+      { name: "arrow", options: { element: arrowElement } },
+    ],
+  });
+
+  popperInstance.update();
+}
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+//
+// hide tooltip logic
+function hideTooltip() {
+  tooltip.removeAttribute("data-show");
+  tooltip.innerHTML = "";
+  buttonShare.classList.remove("is-active");
+  buttonShare.blur();
+}
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+//
+// show tooltip when screen width is >= 48rem
 const minWidth48rem = window.matchMedia("(min-width: 48rem)");
 let popperInstance = null;
 
@@ -27,32 +92,34 @@ function handleWidth(e) {
     popperInstance = null;
   }
 }
-
 minWidth48rem.addEventListener("change", handleWidth);
 handleWidth(minWidth48rem);
 
-//////////////////////////////////////////////////////////////////////////
-//
-// change button state to active when clicked, and back
 buttonShare.addEventListener("click", () => {
   const isShown = tooltip.hasAttribute("data-show");
 
-  if (!isShown) {
-    tooltip.setAttribute("data-show", "");
-    buttonShare.classList.add("is-active");
-    popperInstance.update();
-  } else {
-    tooltip.removeAttribute("data-show");
-    buttonShare.classList.remove("is-active");
-  }
+  if (!isShown) showTooltip();
+  else hideTooltip();
 });
 
-//////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
 //
 // hide tooltip when clicked outside button or tooltip
 document.addEventListener("click", (e) => {
   if (tooltip.hasAttribute("data-show") && !buttonShare.contains(e.target) && !tooltip.contains(e.target)) {
-    tooltip.removeAttribute("data-show");
-    buttonShare.classList.remove("is-active");
+    hideTooltip();
+  }
+});
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+//
+// ESC key logic
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && tooltip.hasAttribute("data-show")) {
+    hideTooltip();
   }
 });
